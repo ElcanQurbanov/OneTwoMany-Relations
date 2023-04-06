32cd7d8081b81d4b1d54bb73123b08189aaa212b
Elcan <eljanng@code.edu.az>
2023-04-06 05:02:13 +0400

    Homework

---
+++ Entity-Framework-Slider/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Entity_Framework_Slider.Data;
+using Entity_Framework_Slider.Models;
+using Entity_Framework_Slider.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Entity_Framework_Slider.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+			_context = context;
+        }
+
+        public async Task<IActionResult> Index()
+		{
+			List<Slider> sliders = await _context.Sliders.ToListAsync();
+
+            //IQueryable<Slider> slide = _context.Sliders.AsQueryable();
+            //List<Slider> query = slide.Where(m => m.Id > 5).ToList();
+
+            //List<Blog>	blogs = await _context.Blogs.Where(m=> !m.SoftDelete && m.Id >2 ).ToListAsync();
+
+            SliderInfo sliderInfo = await _context.SliderInfos.FirstOrDefaultAsync();
+
+			IEnumerable<Blog> blogs = await _context.Blogs.Where(m=> !m.SoftDelete).ToListAsync();
+
+			IEnumerable<Category> categories = await _context.Categories.Where(m => !m.SoftDelete).ToListAsync();
+
+			IEnumerable<Product> products = await _context.Products.Include(m=>m.Images).Where(m => !m.SoftDelete).ToListAsync();
+
+			About about = await _context.Abouts.Include(m => m.Advantages).Where(m => !m.SoftDelete).FirstOrDefaultAsync();
+
+			IEnumerable<Instagram> instagram = await _context.instagrams.Where(m => !m.SoftDelete).ToListAsync();
+
+            IEnumerable<Say> says = await _context.says.Where(m => !m.SoftDelete).ToListAsync();
+
+
+
+
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+			var res = nums.FirstOrDefault();
+			ViewBag.num = res;
+
+			HomeVM model = new()
+			{
+				Sliders = sliders,
+				SliderInfo = sliderInfo,
+				Blogs = blogs,
+				Categories = categories,
+				Products = products
+			};
+			
+			return View(model);
+		}
+	}
+}
